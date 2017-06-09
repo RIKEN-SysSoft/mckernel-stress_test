86daef48e210b41b2a5a9c0cfb0408a5c11d958a
Toshiyuki Takahashi <toshi@metahack.jp>
2017-06-09 13:50:50 +0900

    wip

---
+++ src/signalonfork.c
@@ -0,0 +1,203 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/time.h>
+#include <signal.h>
+#include <pthread.h>
+#include <sys/wait.h>
+#include <linux/futex.h>
+#include <sys/syscall.h>
+
+#if !defined(SINGLE) && !defined(NOSIGNAL)
+#error CPP macro should be defined
+#endif
+
+
+#ifdef MULTI
+#define NUMTHREADS 4
+#else
+#define NUMTHREADS 1
+#endif
+
+int argc;
+char** argv;
+
+
+struct timeval timeBeforeFork;
+struct timeval timeBeforeTest;
+struct timeval timeAfterTest;
+
+
+#define LAPTIME_MS(start, stop) ((stop.tv_sec - start.tv_sec) * 1000 + (stop.tv_usec - start.tv_usec) / 1000)
+
+struct Thread {
+	int tid;
+	pthread_t pthread;
+} thread[NUMTHREADS];
+
+pthread_barrier_t barrier;
+
+void onError(char* message) {
+
+	fprintf(stderr,  "%s: %s: %m", argv[0], message);
+	exit(-1);
+}
+
+
+void* subjectThread(void*);
+
+void createThreads() {
+
+	if (pthread_barrier_init(&barrier, NULL, NUMTHREADS)) {
+		onError("pthread_barrier_init fail");
+	}
+
+	int i;
+	for (i = 1; i < NUMTHREADS; i++) {
+		int rval;
+		thread[i].tid = i;
+		rval = pthread_create(&thread[i].pthread, NULL, subjectThread, &thread[i]);
+		if (rval) {
+			onError("pthread_create fail");
+		}
+	}
+
+	thread[0].tid = 0;
+	thread[0].pthread = pthread_self();
+	subjectThread(&thread[0]);
+}
+
+
+void joinThreads() {
+
+	int i;
+
+	for (i = 1; i < NUMTHREADS; i++) {
+		void* rval;
+		if (pthread_join(thread[i].pthread, &rval)) {
+			onError("pthread_join fail");
+		}  
+	}
+
+	printf("Join done\n");
+}
+
+
+void subjectTask(struct Thread* thread) {
+
+	pthread_barrier_wait(&barrier);
+
+	gettimeofday(&timeBeforeTest, NULL);
+	printf("[%d] setup: %d ms\n", thread->tid, LAPTIME_MS(timeBeforeFork, timeBeforeTest));
+
+	printf("[%d] START TEST\n", thread->tid);
+
+	for(;;) {
+		int pid;
+		pid = fork();
+		if (pid < 0) {
+			onError("fork");
+		} else if (pid == 0) {
+			exit(0);
+		}
+	}
+
+	printf("%d TEST FAIL OVERRUN\n", thread->tid);
+
+	gettimeofday(&timeAfterTest, NULL);
+
+	for(;;);
+//	exit(0);
+}
+
+
+void subjectProcess() {
+
+	printf("[%d] I am a subject.\n", getpid());
+
+//	Subjecttask();
+}
+
+
+void subjectCleanup(void* arg) {
+
+	struct Thread* thread = (struct Thread*) arg;
+	printf("[%d] cleanup\n", thread->tid);
+}
+
+
+void* subjectThread(void* arg) {
+
+	struct Thread* thread = (struct Thread*)arg;
+
+	printf("[%d] I am a subjectThread %d, %x %x\n", getpid(), thread->tid, thread->pthread, pthread_self());
+
+	pthread_cleanup_push(subjectCleanup, arg);
+
+	//sleep(random() % 5 + 1);
+	//printf("[%d:%d] wake up\n", getpid(), thread->tid);
+
+	pthread_barrier_wait(&barrier);
+
+	subjectTask(thread);
+
+	pthread_cleanup_pop(1);
+
+	return NULL;
+}
+
+
+void examinerProcess(pid_t subject) {
+	printf("[%d] I am the examiner for %d.\n", getpid(), subject);
+
+	struct timespec req, rem;
+	req.tv_sec = 0;
+	req.tv_nsec = 500000000; // 500msec
+
+	if (nanosleep(&req, &rem) < 0) {
+		fprintf(stderr, "nanosleep is interrupted, but ignore\n");
+	}
+
+	kill(subject, SIGTERM);
+	if (waitpid(subject, NULL, 0) < 0) {
+		onError("waitpid fail");
+	}
+
+	printf("TEST SUCCESSED IF YOU DID NOT SEE 'OVERRUN'\n");
+	printf("TEST FINISHED\n");
+}
+
+
+int main(int _argc, char** _argv)
+{
+	pid_t pid;
+	
+	argc = _argc;
+	argv = _argv;
+
+	//	setup();
+
+	gettimeofday(&timeBeforeFork, NULL);
+
+#ifdef NOSIGNAL
+	createThreads();
+	joinThreads();
+#else
+	pid = fork();
+	if (pid < 0) {
+		onError("fork");
+	} else if (pid == 0) {
+//		subjectProcess();
+		createThreads();
+		joinThreads();
+	} else {
+		examinerProcess(pid);
+	}
+#endif
+
+	return 0;
+}
