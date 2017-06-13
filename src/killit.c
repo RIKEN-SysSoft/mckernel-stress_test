c3aa4ab05d205979a11c7d64cf155371fa832b63
Toshiyuki Takahashi <toshi@metahack.jp>
2017-06-13 22:25:21 +0900

    options added

--- src/killit.c
+++ src/killit.c
@@ -6,10 +6,17 @@
 #include <signal.h>
 #include <pthread.h>
 #include <sys/wait.h>
+#include <string.h>
+
+#define MAXNUMPROCESSES 256
 
 extern char** environ;
 int argc;
 char** argv;
+int numprocesses = 1;
+int waitmsec = 500; // 500msec
+
+pid_t subjectpid[MAXNUMPROCESSES];
 
 void onError(char* message) {
 
@@ -18,35 +25,46 @@ void onError(char* message) {
 }
 
 
-void examinerProcess(pid_t subject) {
-	printf("[%d] I am the examiner for %d.\n", getpid(), subject);
+void examinerProcess() {
+	printf("[%d] I am the examiner.\n", getpid());
 
 	struct timespec req, rem;
-	req.tv_sec = 0;
-	req.tv_nsec = 500000000; // 500msec
+
+	req.tv_sec = waitmsec / 1000;
+	req.tv_nsec = (waitmsec % 1000) * 1000;
 
 	if (nanosleep(&req, &rem) < 0) {
 		fprintf(stderr, "nanosleep is interrupted, but ignore\n");
 	}
 
-	kill(subject, SIGINT);
+	int i;
 
-	int status;
-	if (waitpid(subject, &status, 0) < 0) {
-		onError("waitpid fail");
+	for (i = 0; i < numprocesses; i++) {
+		if (kill(subjectpid[i], SIGINT) < 0) {
+			printf("[%d] TEST EXITED ALREADY\n", subjectpid[i]);
+			subjectpid[i] = 0;
+		}
 	}
 
-	if (WIFEXITED(status)) {
-		printf("The TEST process exited with return value %d\n", WEXITSTATUS(status));
-		return;
-	}
+	for (i = 0; i < numprocesses; i++) {
+		int status;
+		pid_t pid = subjectpid[i];
 
-	if (WIFSIGNALED(status)) {
-		printf("The TEST process is terminated by the signal %d\n", WTERMSIG(status));
-	}
+		if (pid == 0) continue;
 
-	printf("TEST SUCCESSED IF YOU DID NOT SEE 'OVERRUN'\n");
-	printf("TEST FINISHED\n");
+		if (waitpid(pid, &status, 0) < 0) {
+			onError("waitpid fail");
+		}
+		
+		if (WIFEXITED(status)) {
+			printf("[%d] The TEST process exited with return value %d\n", pid, WEXITSTATUS(status));
+		} else if (WIFSIGNALED(status)) {
+			printf("[%d] The TEST process is terminated by the signal %d\n", pid, WTERMSIG(status));
+		}
+
+		printf("[%d] TEST SUCCESSED IF YOU DID NOT SEE 'OVERRUN'\n", pid);
+		printf("[%d] TEST FINISHED\n", pid);
+	}
 }
 
 
@@ -63,22 +81,67 @@ int main(int _argc, char** _argv)
 		return -1;
 	}
 
-	char** args = calloc(argc, sizeof(char*));
-	int i;
-	for (i = 0; i < argc - 1; i++) {
-		args[i] = argv[i + 1];
+	int i = 1;
+	while (*argv[i] == '-') {
+		if (strcmp("-np", argv[i]) == 0) {
+			i++;
+			if (i < argc) {
+				numprocesses = atoi(argv[i++]);
+				continue;
+			}
+			fprintf(stderr, "%s: num processes required\n", argv[0]);
+			return -1;
+		}
+		if (strcmp("-t", argv[i]) == 0) {
+			i++;
+			if (i < argc) {
+				waitmsec = atoi(argv[i++]);
+				printf("waitmsec %d\n", waitmsec);
+				continue;
+			}
+			fprintf(stderr, "%s: timer value (msec) required\n", argv[0]);
+			return -1;
+		}
+		fprintf(stderr, "%s: invalid option\n", argv[0]);
+		return -1;
 	}
-	args[i] = 0;
 
-	pid = fork();
-	if (pid < 0) {
-		onError("fork");
-	} else if (pid == 0) {
-		execvpe(args[0], args, environ);
-		onError("execve fail");
-	} else {
-		examinerProcess(pid);
+	if (numprocesses < 1 || numprocesses > MAXNUMPROCESSES) {
+		fprintf(stderr, "%s: invalid num processes\n", argv[0]);
+		return -1;
 	}
+
+	if (waitmsec < 0) {
+		fprintf(stderr, "%s: invalid timer value\n", argv[0]);
+		return -1;
+	}
+
+	printf("DANGERTEST KILLIT\n");
+	printf("NUMPROCESSES: %d\n", numprocesses);
+	printf("WAITMSEC: %d\n", waitmsec);
+
+
+
+	char** args = calloc(argc, sizeof(char*));
+	int j = 0;
+	for (; i < argc; i++) {
+		args[j] = argv[i];
+		j++;
+	}
+	args[j] = 0;
+
+	for (i = 0; i < numprocesses; i++) {
+		pid = fork();
+		if (pid < 0) {
+			onError("fork");
+		} else if (pid == 0) {
+			execvpe(args[0], args, environ);
+			onError("execve fail");
+		}
+		subjectpid[i] = pid;
+	}
+
+	examinerProcess();
 
 	return 0;
 }
